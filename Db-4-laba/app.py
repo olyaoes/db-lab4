2c71d79656e4e087b6ee236bbbc3344a6174a7eb
Ubuntu <azureuser@vm-clouds.s3pgksfek1xe3hbm1g0gxzv02e.zx.internal.cloudapp.net>
2025-10-28 17:22:10 +0000

    updated Swagger config and app.yml

--- Db-4-laba/app.py
+++ Db-4-laba/app.py
@@ -1,27 +1,4 @@
-import os
-from waitress import serve
-import yaml
-from flasgger import Swagger
-from flask import jsonify
-from my_project import create_app
-
-DEVELOPMENT_PORT = 1401
-PRODUCTION_PORT = 1401
-HOST = "0.0.0.0"
-DEVELOPMENT = "development"
-PRODUCTION = "production"
-FLASK_ENV = "FLASK_ENV"
-ADDITIONAL_CONFIG = "ADDITIONAL_CONFIG"
-
-def run_flask_app():
-    flask_env = os.getenv(FLASK_ENV, DEVELOPMENT).lower()
-    config_yaml_path = os.path.join(os.getcwd(), 'config', 'app.yml')
-
-    try:
-        with open(config_yaml_path, "r", encoding='utf-8') as yaml_file:
-            config_data_dict = yaml.load(yaml_file, Loader=yaml.FullLoader)
-            additional_config = config_data_dict[ADDITIONAL_CONFIG]
-
+kkkkkkkk
             if flask_env == DEVELOPMENT:
                 config_data = config_data_dict[DEVELOPMENT]
                 app = create_app(config_data, additional_config)
@@ -64,4 +41,5 @@ def run_flask_app():
 
 if __name__ == '__main__':
     run_flask_app()
+
 
